e216f850a4079e191ae9ba6d17915f5336f30909
Casey <caseyly@gmail.com>
2015-10-26 01:28:39 -0400

    initial calendar

--- js/repo.js
+++ js/repo.js
@@ -4,8 +4,8 @@ repo.view = function (ctrl) {
     return m("#repo-list.collection with-header", [
         m(".collection-header", "Choose calendar"),
         ctrl.repos.map(function (item) {
-            if (item.substring(item.lastIndexOf(".")) == ".cw")
-                return m("a.collection-item", { href: "javascript:void(0)" }, item);
+            if (item.substring(item.name.lastIndexOf(".")) == ".cw")
+                return m("a.collection-item", { href: "javascript:void(0)" }, item.name);
         })
     ]);
 }
