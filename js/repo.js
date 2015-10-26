a9f89abc2a14b37d9e24e8ea0dae8937273fc9dc
Casey <caseyly@gmail.com>
2015-10-26 00:53:55 -0400

    initial calendar

--- js/repo.js
+++ js/repo.js
@@ -4,6 +4,6 @@ repo.view = function (ctrl) {
     return m(".collection", [
         ctrl.repos.map(function (item) {
             return m("a.collection-item[href='javascript:void(0)']", item);
-        }
+        })
     ]);
 }
